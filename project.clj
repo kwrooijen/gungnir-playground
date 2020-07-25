5c63e695be7dc0b7cdf952f20c178d2d1034a976
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-07-25 11:59:09 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject gungnir-playground "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Example code for the Gungnir library"
+  :url "https://github.com/kwrooijen/gungnir-playground"
+  :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [kwrooijen/gungnir "0.0.1-SNAPSHOT"]
                  [buddy/buddy-hashers "1.4.0"]
